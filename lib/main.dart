e2e857716d644b7e31ac8bae0956b9cbd1709b9a
nazeef <naseefvaliyakath@gmail.com>
2022-11-24 23:46:23 +0300

    second

--- lib/main.dart
+++ lib/main.dart
@@ -145,34 +145,28 @@ class _MyAppState extends State<MyApp> {
                         child: Text('print receipt(esc)'),
                         onPressed:  _connected?() async {
                           Map<String, dynamic> config = Map();
-
                           List<LineText> list = [];
+                          list.add(LineText(type: LineText.TYPE_TEXT, content: 'A Title', weight: 1, align: LineText.ALIGN_CENTER,linefeed: 1));
+                          list.add(LineText(type: LineText.TYPE_TEXT, content: 'this is conent left', weight: 0, align: LineText.ALIGN_LEFT,linefeed: 1));
+                          list.add(LineText(type: LineText.TYPE_TEXT, content: 'this is conent right', align: LineText.ALIGN_RIGHT,linefeed: 1));
+                          list.add(LineText(type: LineText.TYPE_TEXT, content: 'A Title', weight: 1, align: LineText.ALIGN_CENTER,linefeed: 1));
+                          list.add(LineText(type: LineText.TYPE_TEXT, content: 'this is conent left', weight: 0, align: LineText.ALIGN_LEFT,linefeed: 1));
+                          list.add(LineText(type: LineText.TYPE_TEXT, content: 'this is conent right', align: LineText.ALIGN_RIGHT,linefeed: 1));
+                          list.add(LineText(type: LineText.TYPE_TEXT, content: 'A Title', weight: 1, align: LineText.ALIGN_CENTER,linefeed: 1));
+                          list.add(LineText(type: LineText.TYPE_TEXT, content: 'this is conent left', weight: 0, align: LineText.ALIGN_LEFT,linefeed: 1));
+                          list.add(LineText(type: LineText.TYPE_TEXT, content: 'this is conent right', align: LineText.ALIGN_RIGHT,linefeed: 1));
+                          list.add(LineText(type: LineText.TYPE_TEXT, content: 'A Title', weight: 1, align: LineText.ALIGN_CENTER,linefeed: 1));
+                          list.add(LineText(type: LineText.TYPE_TEXT, content: 'this is conent left', weight: 0, align: LineText.ALIGN_LEFT,linefeed: 1));
+                          list.add(LineText(type: LineText.TYPE_TEXT, content: 'this is conent right', align: LineText.ALIGN_RIGHT,linefeed: 1));
 
-                          list.add(LineText(type: LineText.TYPE_TEXT, content: '**********************************************', weight: 1, align: LineText.ALIGN_CENTER,linefeed: 1));
-                          list.add(LineText(type: LineText.TYPE_TEXT, content: '打印单据头', weight: 1, align: LineText.ALIGN_CENTER, fontZoom: 2, linefeed: 1));
+                          list.add(LineText(linefeed: 1));
+                          list.add(LineText(type: LineText.TYPE_BARCODE, content: 'A12312112', size:10, align: LineText.ALIGN_CENTER, linefeed: 1));
+                          list.add(LineText(linefeed: 1));
+                          list.add(LineText(type: LineText.TYPE_QRCODE, content: 'qrcode i', size:10, align: LineText.ALIGN_CENTER, linefeed: 1));
                           list.add(LineText(linefeed: 1));
 
-                          list.add(LineText(type: LineText.TYPE_TEXT, content: '----------------------明细---------------------', weight: 1, align: LineText.ALIGN_CENTER, linefeed: 1));
-                          list.add(LineText(type: LineText.TYPE_TEXT, content: 'naseeff', weight: 1, align: LineText.ALIGN_LEFT, x: 0,relativeX: 0, linefeed: 0));
-                          list.add(LineText(type: LineText.TYPE_TEXT, content: 'naseeff', weight: 1, align: LineText.ALIGN_LEFT, x: 0,relativeX: 0, linefeed: 0));
 
-                          list.add(LineText(type: LineText.TYPE_TEXT, content: 'naseeff', weight: 1, align: LineText.ALIGN_LEFT, x: 0,relativeX: 0, linefeed: 0));
-
-                          list.add(LineText(type: LineText.TYPE_TEXT, content: 'naseeff', weight: 1, align: LineText.ALIGN_LEFT, x: 0,relativeX: 0, linefeed: 0));
-
-                          list.add(LineText(type: LineText.TYPE_TEXT, content: 'naseeff', weight: 1, align: LineText.ALIGN_LEFT, x: 0,relativeX: 0, linefeed: 0));
-
-                          list.add(LineText(type: LineText.TYPE_TEXT, content: 'മലയാളം ടൈപ്പിംഗ് ...', weight: 1, align: LineText.ALIGN_LEFT, x: 350, relativeX: 0, linefeed: 0));
-                          list.add(LineText(type: LineText.TYPE_TEXT, content: '数量', weight: 1, align: LineText.ALIGN_LEFT, x: 500, relativeX: 0, linefeed: 1));
-
-                          list.add(LineText(type: LineText.TYPE_TEXT, content: '混凝土C30', align: LineText.ALIGN_LEFT, x: 0,relativeX: 0, linefeed: 0));
-                          list.add(LineText(type: LineText.TYPE_TEXT, content: '吨', align: LineText.ALIGN_LEFT, x: 350, relativeX: 0, linefeed: 0));
-                          list.add(LineText(type: LineText.TYPE_TEXT, content: '12.0', align: LineText.ALIGN_LEFT, x: 500, relativeX: 0, linefeed: 1));
-
-                          list.add(LineText(type: LineText.TYPE_TEXT, content: '**********************************************', weight: 1, align: LineText.ALIGN_CENTER,linefeed: 1));
-                          list.add(LineText(linefeed: 1));
-
-                          // ByteData data = await rootBundle.load("assets/images/bluetooth_print.png");
+                          // ByteData data = await rootBundle.load("images/food_delivery.png");
                           // List<int> imageBytes = data.buffer.asUint8List(data.offsetInBytes, data.lengthInBytes);
                           // String base64Image = base64Encode(imageBytes);
                           // list.add(LineText(type: LineText.TYPE_IMAGE, content: base64Image, align: LineText.ALIGN_CENTER, linefeed: 1));
@@ -183,26 +177,7 @@ class _MyAppState extends State<MyApp> {
                       OutlinedButton(
                         child: Text('print label(tsc)'),
                         onPressed:  _connected?() async {
-                          Map<String, dynamic> config = Map();
-                          config['width'] = 40; // 标签宽度，单位mm
-                          config['height'] = 70; // 标签高度，单位mm
-                          config['gap'] = 2; // 标签间隔，单位mm
 
-                          // x、y坐标位置，单位dpi，1mm=8dpi
-                          List<LineText> list = [];
-                          list.add(LineText(type: LineText.TYPE_TEXT, x:10, y:10, content: 'A Title'));
-                          list.add(LineText(type: LineText.TYPE_TEXT, x:10, y:40, content: 'this is content'));
-                          list.add(LineText(type: LineText.TYPE_QRCODE, x:10, y:70, content: 'qrcode i\n'));
-                          list.add(LineText(type: LineText.TYPE_BARCODE, x:10, y:190, content: 'qrcode i\n'));
-
-                          // List<LineText> list1 = [];
-                          // ByteData data = await rootBundle.load("assets/images/guide3.png");
-                          // List<int> imageBytes = data.buffer.asUint8List(data.offsetInBytes, data.lengthInBytes);
-                          // String base64Image = base64Encode(imageBytes);
-                          // list1.add(LineText(type: LineText.TYPE_IMAGE, x:10, y:10, content: base64Image,));
-
-                          await bluetoothPrint.printLabel(config, list);
-                          //await bluetoothPrint.printLabel(config, list1);
                         }:null,
                       ),
                       OutlinedButton(
